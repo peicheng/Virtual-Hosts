f7ab147022b6e42b8633adb3ced89b7aade60fcd
peicheng <peicheng5@gmail.com>
2025-03-01 16:15:35 +0800

    Update VhostsService.java threads 30

--- app/src/main/java/com/github/xfalcon/vhosts/vservice/VhostsService.java
+++ app/src/main/java/com/github/xfalcon/vhosts/vservice/VhostsService.java
@@ -114,7 +114,7 @@ public class VhostsService extends VpnService {
             networkToDeviceQueue = new ConcurrentLinkedQueue<>();
             udpSelectorLock = new ReentrantLock();
             tcpSelectorLock = new ReentrantLock();
-            executorService = Executors.newFixedThreadPool(20);
+            executorService = Executors.newFixedThreadPool(30);
             executorService.submit(new UDPInput(networkToDeviceQueue, udpSelector, udpSelectorLock));
             executorService.submit(new UDPOutput(deviceToNetworkUDPQueue, networkToDeviceQueue, udpSelector, udpSelectorLock, this));
             executorService.submit(new TCPInput(networkToDeviceQueue, tcpSelector, tcpSelectorLock));
